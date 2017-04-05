5178cb35ec94c860144113ebfaacd27055f7d047
Johan <jmarchan@jockeyclub.com.ar>
2017-04-05 17:28:33 -0300

    quita comentarios innecesarios

--- database/seeds/AreaTableSeeder.php
+++ database/seeds/AreaTableSeeder.php
@@ -12,32 +12,6 @@ class AreaTableSeeder extends Seeder
      */
     public function run()
     {
-
-//
-//        $area = new Area();
-//        $area->name = 'Proveedores';
-//        $area->description = 'Esta es el área que le paga a los proveedores';
-//        $area->email = 'proveedores@jockeyclub.com.ar';
-//        $area->fax = '1122';
-//        $area->sector_id= 1;
-//        $area->save();
-//
-//        $area = new Area();
-//        $area->name = 'Contabilidad';
-//        $area->description = 'Esta es el área que lleva los libros contables';
-//        $area->email = 'contabilidad@jockeyclub.com.ar';
-//        $area->fax = '1133';
-//        $area->sector_id= 1;
-//        $area->save();
-//
-//        $area = new Area();
-//        $area->name = 'Desarrollo';
-//        $area->description = 'Esta es el área que hace las aplicaciones';
-//        $area->email = 'desarrollo@jockeyclub.com.ar';
-//        $area->fax = '1442';
-//        $area->sector_id= 2;
-//        $area->save();
-
         $area = new Area();
         $area->name = 'Infraestructura';
         $area->description = 'Esta es el área que instala los equipos';
